d3445cf3fe02c768b9fb46631c09a8ac647731d6
unworthyEnzyme <unworthy_enzyme@protonmail.com>
2025-04-04 13:26:33 +0300

    Seed the database with Role data.

--- Backend/ECommerceApp.DataAccess/Concrete/EntityFramework/AppDbContext.cs
+++ Backend/ECommerceApp.DataAccess/Concrete/EntityFramework/AppDbContext.cs
@@ -1,26 +1,31 @@
 using ECommerceApp.Entities.Concrete;
 using Microsoft.EntityFrameworkCore;
 
-namespace ECommerceApp.DataAccess.Concrete.EntityFramework
-{
-    public class AppDbContext : DbContext
-    {
+namespace ECommerceApp.DataAccess.Concrete.EntityFramework {
+    public class AppDbContext : DbContext {
         public DbSet<User> Users { get; set; }
         public DbSet<Role> Roles { get; set; }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
             optionsBuilder.UseSqlServer("Server=localhost,1433;Database=ECommerceDb;User Id=sa;Password=YourStrong!Passw0rd;TrustServerCertificate=True;");
         }
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
+        protected override void OnModelCreating(ModelBuilder modelBuilder) {
             modelBuilder.Entity<User>()
                 .HasOne(u => u.Role)
                 .WithMany(r => r.Users)
                 .HasForeignKey(u => u.RoleId);
 
             base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Role>().HasData(
+                new Role {
+                    RoleId = 1,
+                    Name = "Admin",
+                },
+                new Role {
+                    RoleId = 2,
+                    Name = "User",
+                });
         }
     }
 }
---
+++ Backend/ECommerceApp.DataAccess/Migrations/20250404102237_SeedRoles.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace ECommerceApp.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedRoles : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Roles",
+                columns: new[] { "RoleId", "CreatedAt", "DeletedAt", "IsActive", "Name", "UpdatedAt" },
+                values: new object[,]
+                {
+                    { 1, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), null, false, "Admin", null },
+                    { 2, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), null, false, "User", null }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Roles",
+                keyColumn: "RoleId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Roles",
+                keyColumn: "RoleId",
+                keyValue: 2);
+        }
+    }
+}

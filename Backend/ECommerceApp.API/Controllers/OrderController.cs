17940111f902eea99adb2f49e7a825acacdb4e5f
unworthyEnzyme <unworthy_enzyme@protonmail.com>
2025-05-29 13:12:28 +0300

    Fix messed up formatting

--- Backend/ECommerceApp.API/Controllers/OrderController.cs
+++ Backend/ECommerceApp.API/Controllers/OrderController.cs
@@ -11,7 +11,9 @@ namespace ECommerceApp.API.Controllers
   [Authorize]
   public class OrderController : ControllerBase
   {
-    private readonly IOrderService _orderService; public OrderController(IOrderService orderService)
+    private readonly IOrderService _orderService;
+
+    public OrderController(IOrderService orderService)
     {
       _orderService = orderService;
     }
